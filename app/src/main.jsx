3fa2cb0803e865e4738364d93b9ab1edd40d37ee
Sunny Dowari <86121971+sunnydowari@users.noreply.github.com>
2023-08-18 01:29:56 +0530

    config for eslint and tailwind

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
->>>>>>> 69b8c8243e29f06b2953ce98cec8720167bd0b23
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
-);
-=======
 )
->>>>>>> 69b8c8243e29f06b2953ce98cec8720167bd0b23
