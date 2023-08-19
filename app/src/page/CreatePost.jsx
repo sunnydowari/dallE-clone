884cb75a3ce8d1a111bc394c8097718d824008a4
Sunny Dowari <86121971+sunnydowari@users.noreply.github.com>
2023-08-19 12:31:28 +0530

    fixing deployment routes

--- app/src/page/CreatePost.jsx
+++ app/src/page/CreatePost.jsx
@@ -29,7 +29,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         //set this according to the website that is requesting generate image..
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://dalle-back.vercel.app/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -57,7 +57,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dalle-back.vercel.app/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
